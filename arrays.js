5b28ca01e4298a2351deab2b7ca3b71a62d2dcb6
Amy Cai <amycai89@gmail.com>
2020-05-20 01:09:08 +0000

    completed 1-3 in arrays and functions

--- arrays.js
+++ arrays.js
@@ -3,17 +3,38 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+    for(let i = 0; i < items.length; i++){
+        console.log(items[i] + ' '+ i); 
+    }
 }
+
+printIndices(['apple', 'berry', 'cherry'])
+//or use for(i in items)
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+
+let result = []; 
+
+  for(let i in items){
+    if(i % 2 === 0){
+        result.push(items[i]); 
+    }
+  }
+  console.log(result)
 }
+
+everyOtherItem(['apple', 'berry', 'cherry'])
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+let result = []; 
+
+for(let i in items){
+
+    }
 }
+
+
--- functions.js
+++ functions.js
@@ -5,11 +5,27 @@
 
 // Define your function here
 
+function isHomeTown(town){
+    if(town === 'San Francisco'){
+        return 'True'; 
+    }
+
+    else{
+        return 'False'; 
+    }
+}
+
+isHomeTown('San Francisco')
 
 // 2. getFullName
 
 // Define your function here
 
+function getFullName(firstName, lastName){
+    return firstName + ' ' + lastName; 
+}
+
+getFullName('Jane', 'Doe')
 
 // 3. calculateTotal
 
